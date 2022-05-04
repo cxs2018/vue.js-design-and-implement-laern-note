fe4ae13de170c11937c4547e678350cae9e8d05d
cuixuesen <2821436626@qq.com>
2022-05-04 11:41:57 +0800

    feat: init

---
+++ 第4章-响应系统的作用与实现/4.3-设计一个完善的响应系统.js
@@ -0,0 +1,48 @@
+let activeEffect;
+
+function effect(fn) {
+  activeEffect = fn;
+  fn();
+}
+
+const data = { text: "Hello, world!" };
+
+const bucket = new WeakMap();
+
+function track(target, key) {
+  if (!activeEffect) return;
+  let depsMap = bucket.get(target);
+  if (!depsMap) {
+    bucket.set(target, (depsMap = new Map()));
+  }
+  let deps = depsMap.get(key);
+  if (!deps) {
+    depsMap.set(key, (deps = new Set()));
+  }
+  deps.add(activeEffect);
+}
+
+function trigger(target, key) {
+  const depsMap = bucket.get(target);
+  if (!depsMap) return;
+  const effects = depsMap.get(key);
+  effects && effects.forEach((fn) => fn());
+}
+
+const obj = new Proxy(data, {
+  get(target, key) {
+    track(target, key);
+    return target[key];
+  },
+  set(target, key, newVal) {
+    target[key] = newVal;
+    trigger(target, key);
+    return true;
+  },
+});
+
+effect(() => {
+  console.log("我读取了响应式数据data.text: ", obj.text);
+});
+
+obj.text = "你好，世界！";
